32bded7d6c70c3eccf715ebee8d1e1fc76efdc21
Davids0da <david@mvc.studio>
2023-10-27 00:39:54 +0200

    moved files

--- src/components/movement/arts/components/ButtonWithIcon.tsx
+++ src/components/movement/arts/components/ButtonWithIcon.tsx
@@ -1,24 +1,14 @@
-import React, { FC } from "react";
-import { Link } from "react-router-dom";
-import literature from "/src/assets/arts/literature.svg";
-import music from "/src/assets/arts/music.svg";
-import paintings from "/src/assets/arts/paintings.svg";
-import photography from "/src/assets/arts/photography.svg";
-import publications from "/src/assets/arts/publications.svg";
-import video from "/src/assets/arts/video.svg";
+import React, {FC} from "react";
+import {Link} from "react-router-dom";
 
 interface ButtonWithIconProps {
     type: string;
 }
-
-const types = [music, paintings, literature ,photography, publications, video];
-
-export const ButtonWithIcon: FC<ButtonWithIconProps> = ({ type }) => {
-    const buttonImg = types.find(xd => xd.toString() === `/src/assets/arts/${type}.svg`);
+export const ButtonWithIcon:FC<ButtonWithIconProps> = ({type}) => {
     return <Link to={type}><div className="hover:scale-[1.2] duration-200 grid place-items-center h-14 w-14 rounded-full ring-2 ring-white border-black border-solid border">
-        <img className="h-8 w-8"
-            src={buttonImg}
-            alt="" />
-    </div>
+            <img className="h-8 w-8"
+                 src={`/arts/${type}.svg`}
+                 alt="" />
+        </div>
     </Link>
 };
