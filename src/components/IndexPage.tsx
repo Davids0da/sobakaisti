e7ce3d6de3a2eb434a41f3687742c6123e577743
Davids0da <david@mvc.studio>
2024-05-27 15:45:21 +0200

    dodata sifra

--- src/components/IndexPage.tsx
+++ src/components/IndexPage.tsx
@@ -5,10 +5,25 @@ import { DogWithZoom } from "./DogZoomWithText";
 
 export const IndexPage: FC = () => {
 
-    const [visible, setVisible] = useState(true);
+    const [visible, setVisible] = useState(false);
+
+    const handleChange = (event: any) => {
+        if (event?.target.value === 'sobakaisti123') {
+            setVisible(true);
+        }
+    }
+
+
 
     return <div className="grid place-items-center h-screen">
         <div className="flex w-full">
+            {!visible && <input
+                type="text"
+                className="block w-full rounded-md border-gray-300 pl-3 pr-12 focus:border-green-600 focus:ring-green-600 sm:text-sm"
+                placeholder="Unesite lozinku za ulaz."
+                onChange={handleChange}
+            />
+            }
             <div id="movement-dog-icon" className="w-1/2">
                 {visible && <DogWithZoom image_src={MovementDog} label="POKRET" url="movement" />}
             </div>
