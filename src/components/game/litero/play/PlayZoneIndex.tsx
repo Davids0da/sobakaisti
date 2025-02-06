8c58f6bafe4b317fd9629d749a7937902ac36b20
Davids0da <david@mvc.studio>
2025-02-06 15:11:10 +0100

    remove loader

--- src/components/game/litero/play/PlayZoneIndex.tsx
+++ src/components/game/litero/play/PlayZoneIndex.tsx
@@ -12,7 +12,7 @@ export interface PlayZoneIndexProps {
 
 export const PlayZoneIndexPage: FC<PlayZoneIndexProps> = ({ type }) => {
 
-    const { data, isLoading } = useSWR(`/game-articles?filters[type][$eq]=${type}&populate=*&sort=createdAt:desc`, getGameArticlesFetcher);
+    const { data } = useSWR(`/game-articles?filters[type][$eq]=${type}&populate=*&sort=createdAt:desc`, getGameArticlesFetcher);
 
 
     const [visible, setVisible] = useState(4);
@@ -34,9 +34,8 @@ export const PlayZoneIndexPage: FC<PlayZoneIndexProps> = ({ type }) => {
         <GameDogAndSocialIconsHeader />
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
             <h1 className="font-sobakaisti text-center text-3xl mb-16">{type}</h1>
-            {isLoading && <Loader />}
             <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                {!isLoading &&
+                {
                     data
                         ?.filter((item) => item?.attributes?.hidden === false) // Client-side filter for hidden === false
                         .slice(0, visible)
