244f396a82895c3e13a0ff3f75be269a8cd9209d
Davids0da <david@mvc.studio>
2025-04-11 09:38:45 +0200

    fix CV

--- src/components/game/txt/CvIndex.tsx
+++ src/components/game/txt/CvIndex.tsx
@@ -8,12 +8,21 @@ export const CvIndexPage: FC = () => {
 
     const { data } = useSWR(`/cv`, getCvFetcher);
 
+    const ApiUrl = import.meta.env.VITE_BASE_URL;
+
+    function createMarkup() {
+        const htmlContent = { __html: data?.attributes.content };
+        const re = /<img src="/gm
+        const newStr = htmlContent.__html?.replace(re, `<img src="${ApiUrl}`) || "";
+        const htmlContentRender = { __html: newStr };
+        return htmlContentRender
+    }
 
     return <div className="bg-white">
         <GameDogAndSocialIconsHeader />
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
             <h1 className="font-sobakaisti text-center text-3xl mb-16">CV</h1>
-            {data?.attributes.content}
+            <p dangerouslySetInnerHTML={createMarkup()}></p>
         </div>
         <GameFooter />
     </div>
